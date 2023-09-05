1398aae6d5abc329bea11242f92312c8ec24d717
roniky <roniky93@gmail.com>
2023-09-05 14:50:32 -0300

    #

--- script.js
+++ script.js
@@ -24,10 +24,16 @@ window.onscroll = () => {
             });
         };
     });
-               /*---------NavBar-----------*/
+            
+    /*---------NavBar-----------*/
 let header = document.querySelector('header');
 
 header.classList.toggle('sticky', window.scrollY > 100);
 
+
+//*=========== remove toggle icon and navbar when click navbar link (scroll) ============*/
+
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active')
 
 };
