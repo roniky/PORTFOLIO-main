1116e14852c96e8339d3cd7305a62677f08ddd88
roniky <roniky93@gmail.com>
2023-09-05 14:35:58 -0300

    #

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+let sections = document.querySelectorAll('#menu-icon');
+let navLinks = document.querySelectorAll('.navbar');
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
